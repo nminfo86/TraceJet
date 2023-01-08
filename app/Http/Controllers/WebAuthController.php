1c2588e5c9ad10a64bbd7944d8a6e34f55ff6a56
Samir DJENNAT <djennat.samir@grp-sonelgaz.sng>
2023-01-08 14:34:03 +0100

    add web routing

---
+++ app/Http/Controllers/WebAuthController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use PDOException;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
+use App\Http\Requests\AccessTokensRequest;
+use App\Http\Controllers\Api\v1\AccessTokensController;
+use GuzzleHttp\Psr7\Request;
+
+class WebAuthController extends AccessTokensController
+{
+
+    /* this function is used to authenticate user with session and to get token */
+
+    public function webLogin(AccessTokensRequest $request)
+    {
+        $credentials = $request->only('username', 'password');
+        if (Auth::attempt($credentials)) {
+            $content=$this->login($request)->getContent();
+            $response=json_decode($content,true);
+            //dd($response['message']['token']);
+            $request->session()->put('token',$response['token']);
+            return redirect()->intended('/');
+        }
+       return redirect("/")->with('error','Login details are not valid');
+    }
+
+    public function webLogout()
+    {
+        Session::flush();
+        Auth::guard('web')->logout();
+        Auth::guard('sanctum')->guest();
+        return redirect('/');
+    }
+}
