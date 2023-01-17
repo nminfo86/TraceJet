a6d5dacfb00e883dc5d26e88e39780512909c3c7
samir03121990 <91221274+samir03121990@users.noreply.github.com>
2023-01-17 20:38:55 +0100

    logout

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -61,16 +61,14 @@
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle text-muted waves-effect waves-dark pro-pic" href="#"
                         id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        <img src="../assets/images/users/profile.png" alt="user" class="rounded-circle"
-                            width="31">
+                        {{-- <img src="../assets/images/users/profile.png" alt="user" class="rounded-circle" width="31"> --}}
+                        <i class="mdi mdi-face-profile text-muted fa-2x"></i>
                     </a>
                     <ul class="dropdown-menu dropdown-menu-end user-dd animated" aria-labelledby="navbarDropdown">
                         <a class="dropdown-item" href="javascript:void(0)"><i class="ti-user m-r-5 m-l-5"></i>
-                            My Profile</a>
-                        <a class="dropdown-item" href="javascript:void(0)"><i class="ti-wallet m-r-5 m-l-5"></i>
-                            My Balance</a>
-                        <a class="dropdown-item" href="javascript:void(0)"><i class="ti-email m-r-5 m-l-5"></i>
-                            Inbox</a>
+                            {{ __('Mon profil') }}</a>
+                        <a class="dropdown-item" href="{{ url('logout') }}"><i class="ti-lock m-r-5 m-l-5"></i>
+                            {{ __('Se déconnecter') }}</a>
                     </ul>
                 </li>
                 <!-- ============================================================== -->
