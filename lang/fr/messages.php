d8043a3e26cf3cd770b4b66648acec72cfdaefa7
BoualemCHDev <chblm.dev@gmail.com>
2023-04-06 14:17:52 +0100

    add lang

--- lang/fr/messages.php
+++ lang/fr/messages.php
@@ -3,5 +3,11 @@
 // lang/en/messages.php
 
 return [
-    'List' => 'voir',
+    'operator' => "operateur",
+    'Product not found' => 'Le produit n\'appartient pas à l\'OF actuel',
+    'invalid host' => "hôte invalide, veuillez contacter l'administrateur",
+    'Product exists' => "Le produit existe sur cet hôte",
+    'last_movement_qr' => "Vérifier le produit sur :host",
+    'nok' => "Produit NOK",
+
 ];
