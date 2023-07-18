9515de9e571476d49ef2c324efe80b82e5b9796c
BoualemChDev <chblm.dev@gmail.com>
2023-07-18 14:37:29 +0100

    e

--- app/Http/Controllers/Api/v1/DashboardController.php
+++ app/Http/Controllers/Api/v1/DashboardController.php
@@ -6,7 +6,6 @@ use stdClass;
 use App\Models\Of;
 use App\Models\Post;
 use App\Models\Movement;
-use Illuminate\Support\Arr;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -15,29 +14,30 @@ class DashboardController extends Controller
     public function index(Request $request)
     {
 
-        // $request["caliber_id"] = "";
-        $posts = Post::whereSectionId($request->section_id)->join('posts_types', 'posts.posts_type_id', '=', 'posts_types.id')->get(["posts.id", "post_name", "icon", "color"]);
+        // Fetch posts based on section ID
+        $posts = Post::whereSectionId($request->section_id)
+            ->join('posts_types', 'posts.posts_type_id', '=', 'posts_types.id')
+            ->get(["posts.id", "post_name", "icon", "color"]);
 
+        // Check if no posts found
+        if ($posts->isEmpty()) {
+            return $this->sendResponse("This section does not have any posts", status: false);
+        }
 
-        // Apply condition: Filter by 'start_date' and 'end_date' if present in the request
-        $fpy = Movement::join('serial_numbers', 'movements.serial_number_id', '=', 'serial_numbers.id')
-
-            ->when($request->start_date !== "" && $request->end_date !== "", function ($query) use ($request) {
+        // Fetch FPY (First Pass Yield) data for movements
+        $fpy = Movement::withWhereHas('Posts', fn ($q) => $q->whereSectionId($request->section_id))
+            ->join('serial_numbers', 'movements.serial_number_id', '=', 'serial_numbers.id')
+            ->when($request->start_date  && $request->end_date, function ($query) use ($request) {
                 return $query->whereBetween('movements.created_at', [$request->start_date, $request->end_date]);
             })
-
             ->when($request->caliber_id, function ($query) use ($request) {
                 $query->join('ofs', 'serial_numbers.of_id', '=', 'ofs.id')->where('caliber_id', $request->caliber_id);
             })
-
-
             ->when($request->of_id, function ($query) use ($request) {
                 $query->where('serial_numbers.of_id', $request->of_id);
             })
-
-
             ->where(function ($query) {
-                // Filter out duplicate rows based on the minimum id
+
                 $query->whereRaw('movements.id = (SELECT MIN(id) FROM movements AS m WHERE m.movement_post_id = movements.movement_post_id AND m.serial_number_id = movements.serial_number_id)');
             })
             ->select('movement_post_id as id')
@@ -52,21 +52,18 @@ class DashboardController extends Controller
 
         foreach ($posts as $post) {
             $found = false;
-
+            // Check if the post exists in the fpy collection
             foreach ($fpy as &$item) {
-                // dd($post['id'] === $item["id"]);
-
                 if ($post->id === $item->id) {
+                    // Update the item with post details
                     $item->post_name = $post->post_name;
                     $item->icon = $post->icon;
                     $item->color = $post->color;
                     $found = true;
-                    //dd($item);
                     break;
                 }
             }
-            //dd($fpy);
-
+            // If post is not found, create a new object and add it to the fpy collection
             if (!$found) {
                 $obj = new stdClass();
                 $obj->id = $post->id;
@@ -80,14 +77,13 @@ class DashboardController extends Controller
             }
         }
 
+        // Calculate total FPY for the chain if fpy collection is not empty
         if ($fpy) {
             // Calculate total FPY for the chain
             $total_fpy = collect($fpy)->reduce(fn ($carry, $item) => $carry * ($item->FPY / 100), 1) * 100;
             return $this->sendResponse(data: compact("fpy", "total_fpy"));
         }
-        return $this->sendResponse("Empty FPY", status: false);
-
-        // Return the results
+        // return $this->sendResponse("Empty FPY", status: false);
     }
 
 
