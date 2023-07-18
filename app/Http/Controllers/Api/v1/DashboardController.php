09f9106ad1be5e74b49162930c4844acacdb15c6
BoualemChDev <chblm.dev@gmail.com>
2023-07-18 13:15:39 +0100

    add color and icon to all fpy

--- app/Http/Controllers/Api/v1/DashboardController.php
+++ app/Http/Controllers/Api/v1/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api\v1;
 
+use stdClass;
 use App\Models\Of;
 use App\Models\Post;
 use App\Models\Movement;
@@ -15,17 +16,15 @@ class DashboardController extends Controller
     {
 
         // $request["caliber_id"] = "";
-        $posts = Post::whereSectionId($request->section_id)->get(["id", "post_name"]);
+        $posts = Post::whereSectionId($request->section_id)->join('posts_types', 'posts.posts_type_id', '=', 'posts_types.id')->get(["posts.id", "post_name", "icon", "color"]);
 
 
         // Apply condition: Filter by 'start_date' and 'end_date' if present in the request
-        $fpy = Movement::join('posts', 'movements.movement_post_id', '=', 'posts.id')
-            ->join('posts_types', 'posts.posts_type_id', '=', 'posts_types.id')
-            ->join('serial_numbers', 'movements.serial_number_id', '=', 'serial_numbers.id')
+        $fpy = Movement::join('serial_numbers', 'movements.serial_number_id', '=', 'serial_numbers.id')
 
-            // ->when($request->start_date !== "" && $request->end_date !== "", function ($query) use ($request) {
-            //     return $query->whereBetween('movements.created_at', [$request->start_date, $request->end_date]);
-            // })
+            ->when($request->start_date !== "" && $request->end_date !== "", function ($query) use ($request) {
+                return $query->whereBetween('movements.created_at', [$request->start_date, $request->end_date]);
+            })
 
             ->when($request->caliber_id, function ($query) use ($request) {
                 $query->join('ofs', 'serial_numbers.of_id', '=', 'ofs.id')->where('caliber_id', $request->caliber_id);
@@ -41,39 +40,47 @@ class DashboardController extends Controller
                 // Filter out duplicate rows based on the minimum id
                 $query->whereRaw('movements.id = (SELECT MIN(id) FROM movements AS m WHERE m.movement_post_id = movements.movement_post_id AND m.serial_number_id = movements.serial_number_id)');
             })
-            // ->join('posts', 'movements.movement_post_id', '=', 'posts.id')
-            // ->join('serial_numbers', 'movements.serial_number_id', '=', 'serial_numbers.id')
-            ->select('posts.id', 'posts.post_name', "posts.posts_type_id", 'icon')
+            ->select('movement_post_id as id')
             ->selectRaw('COUNT(IF(movements.result = "OK", 1, NULL)) AS count_ok')
             ->selectRaw('COUNT(IF(movements.result = "NOK", 1, NULL)) AS count_nok')
             ->selectRaw('CAST((COUNT(CASE WHEN movements.result = "OK" THEN 1 END) / COUNT(*)) * 100 AS UNSIGNED) AS FPY')
-            ->groupBy('movements.movement_post_id', 'posts.post_name', "posts.id", "posts.posts_type_id", 'icon')
+            ->groupBy('movement_post_id')
             ->get();
 
+        // $arr = [];
+        $arr = new stdClass();
 
+        foreach ($posts as $post) {
+            $found = false;
 
-        $fpyPosts = $fpy->pluck('post_name')->toArray();
+            foreach ($fpy as &$item) {
+                // dd($post['id'] === $item["id"]);
 
-        $unmatchedPosts = $posts->reject(function ($post) use ($fpyPosts) {
-            return in_array($post['post_name'], $fpyPosts);
-        });
+                if ($post->id === $item->id) {
+                    $item->post_name = $post->post_name;
+                    $item->icon = $post->icon;
+                    $item->color = $post->color;
+                    $found = true;
+                    //dd($item);
+                    break;
+                }
+            }
+            //dd($fpy);
 
-        $unmatchedPosts = $unmatchedPosts->values()->each(function ($unmatchedPost) {
-            $unmatchedPost->count_ok = 0;
-            $unmatchedPost->count_nok = 0;
-            $unmatchedPost->FPY = 0;
-        });
+            if (!$found) {
+                $obj = new stdClass();
+                $obj->id = $post->id;
+                $obj->count_ok = 0;
+                $obj->count_nok = 0;
+                $obj->FPY = 0;
+                $obj->post_name = $post->post_name;
+                $obj->icon = $post->icon;
+                $obj->color = $post->color;
+                $fpy[] = $obj;
+            }
+        }
 
-        $fpy = Arr::collapse([$fpy, $unmatchedPosts]);
-
-
-        // return ($unmatchedPosts);
         if ($fpy) {
-
-
-
-            // dd($fpy);
-            # code...
             // Calculate total FPY for the chain
             $total_fpy = collect($fpy)->reduce(fn ($carry, $item) => $carry * ($item->FPY / 100), 1) * 100;
             return $this->sendResponse(data: compact("fpy", "total_fpy"));
