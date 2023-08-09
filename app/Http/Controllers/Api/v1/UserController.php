fc28e0b866265d5d62cd35eb01ddf5828899224e
BoualemChDev <chblm.dev@gmail.com>
2023-08-09 09:35:45 +0100

    roles_name

--- app/Http/Controllers/Api/v1/UserController.php
+++ app/Http/Controllers/Api/v1/UserController.php
@@ -60,7 +60,7 @@ class UserController extends Controller
                 'name'          => $request->name,
                 'email'         => $request->email,
                 'password'      => Hash::make($request->password),
-                'roles_name' =>  [$request->roles_name],
+                'roles_name' =>  $request->roles_name,
                 'status' =>  $request->status,
             ]);
 
